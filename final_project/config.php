3b4cae7f4f4b2690301ceda74cae05c6370de4a8
Desiree Khoo <dhkhoo@usc.edu>
2021-05-11 17:37:41 -0700

    Update config.php

--- final_project/config.php
+++ final_project/config.php
@@ -1,8 +1,9 @@
 <?php
 session_start(); // put it here so session is started on every page that requries the config
-define('DB_HOST', '');
-define("DB_USER", "");
-define("DB_PASS", "");
-define("DB_NAME", "");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+define("DB_HOST", $url["host"]);
+define("DB_USER", $url["user"]);
+define("DB_PASS", $url["pass"]);
+define("DB_NAME", substr($url["path"],1));
 
 ?>
